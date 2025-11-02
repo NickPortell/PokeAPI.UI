b93fb07b5555017f88e2f85abd8bcc047c1cd42d
Nick Portell <47723468+NickPortell@users.noreply.github.com>
2025-11-01 20:06:03 -0400

    Created nav types and refactored routing (#8)

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import App from './App.tsx'
-import { BaseClient } from "./shared/baseApiClient/baseClient.ts"
-import PokeView from './routes/pokeview.tsx';
-
-async function loadPokeDex(): Promise<[key: string, value: string][]> {
-    return await BaseClient('GET', '/Pokemon/Dex')
-        .then(res => res.json())
-        .then(json => Object.entries<string>(json))
-}
-
-const pokeDex = await loadPokeDex();
-const router = createBrowserRouter(
-    [
-        { path: '/', element: <App/> },
-        { path: '/Viewer', element: <PokeView entries={pokeDex}/> }
-    ]
-);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </StrictMode>
 )
